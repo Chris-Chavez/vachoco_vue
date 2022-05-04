6b4969612fcc624108182c4940b3ca4ab11ce7ea
Chris-Chavez <17171340@itculiacan.edu.mx>
2022-05-03 23:25:23 -0600

    Agregar llamada endpoint api

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-  },
-  getters: {
+    Products:[],
+    Personal:[],
+    Tickets:[],
+    loading: false,
   },
   mutations: {
+    SET_PRODUCTS(state, Products) {
+      state.Products = Products
+    },
   },
   actions: {
+    setProducts({commit}){
+      commit('SET_LOADING', true)
+      axios.get('http://localhost:3000/Products')
+      .then( response => {
+        commit('SET_PRODUCTS', response.data)
+      })
+      .finally(() => commit('SET_LOADING', false))
+    },
   },
-  modules: {
+  getters: {
+    allProducts: state => {
+      return state.Products
+    },
+  },
+  modules:{
+
   }
 })
